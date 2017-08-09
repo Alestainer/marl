c389f4979bc40a41575a92614ee9b8a49170b945
alestainer <nikolaev.anton@phystech.edu>
2017-08-08 21:27:43 -1000

    Specified worker methods

--- marl/learning_algorithm.py
+++ marl/learning_algorithm.py
@@ -1,8 +1,16 @@
 __author__ = 'alestainer'
 """This file contain set of learning algorithms for the game"""
 
-class algorithm():
+import tensorflow as tf
+import numpy as np
+
+class Worker():
 
     def __init__(self):
         pass
 
+    def train(self):
+        pass
+
+    def work(self):
+        pass
